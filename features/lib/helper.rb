062d03579edb458db1b46d1c74be5974a7dffd5b
Alex Mikitenko <nonkor@gmail.com>
2017-06-12 21:31:55 +0300

    [experimental] Add Travis to the project

--- features/lib/helper.rb
+++ features/lib/helper.rb
@@ -1,6 +1,23 @@
 module Helper
   def settings
-    YAML.load(File.open('config/settings.yml'))
+    @settings ||= YAML.load(File.open('config/settings.yml'))
+  end
+
+  def launch_browser(options = {})
+    browser_name = settings[:browser_name]
+
+    case browser_name
+    when :chrome
+      options[:prefs] ||= { credentials_enable_service: false, profile: { password_manager_enabled: false } }
+      #options[:switches] ||= %w(--headless --disable-gpu --remote-debugging-port=9222)
+    when :firefox
+      unless options[:profile]
+        options[:profile] = Selenium::WebDriver::Firefox::Profile.new
+        options[:profile]['dom.webnotifications.enabled'] = false
+      end
+    end
+
+    @browser = Watir::Browser.new browser_name, options
   end
 
   def browser
